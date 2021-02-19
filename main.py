2a9f078ab30557f12cab295fe0445fef6eb404fd
Valentin5600 <79209322+Valentin5600@users.noreply.github.com>
2021-02-19 11:55:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+"""s
+    parser1.0
+    analyseur de texte d'article scientifique
+    syntaxe :
+        parser10.py [arg]
+        [arg] : fichier pdf, -help
+    return :
+        *.txt
+"""
+import os
+import sys
+
+from parse_help import help
+from parser_functions import *
+
+       
+if not sys.argv.__len__()!=1:
+    print("syntaxe : \n parser10.py  [arg] \n [arg] : repertoire , -help")
+
+elif sys.argv[1]=="-help":
+    help()
+
+else :
+    if os.path.exists(sys.argv[1])==False:
+        print("le chemin spécifié ne correspond pas à un repertoire")
+    else:
+        conversions(sys.argv[1])
