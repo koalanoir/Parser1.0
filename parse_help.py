9fbe27d1450b504373a2ac740a6c94c1f5cf338c
Valentin5600 <79209322+Valentin5600@users.noreply.github.com>
2021-03-15 21:03:05 +0100

    Delete parse_help.py

--- parse_help.py
+++
@@ -1,5 +0,0 @@
-"""
-    help for use parse
-"""
-def help():
-    print("parse est un outil d'aide à l'analyse de textes scientifiques.\nCe programme accepte en entrée un dossier contenant des fichiers au format PDF.\nLe résultat obtenue est un sous-dossier contenant un fichier au format texte pour chaque fichier au format PDF.\nCes sorties texte portent le même nom que l'entrée PDF et contiennent le nom du fichier d'origine, le titre du papier et le résumé de l'auteur")
